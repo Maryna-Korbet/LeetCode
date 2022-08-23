1998df131f886b01d7c469d83e8daa30062c81e3
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2022-08-23 18:00:16 +0000

    Deploying to gh-pages from @ Maryna-Korbet/leetcode@073f934d0f72a59a4c39e3fc6ec74101a97ba86a 🚀

---
+++ index.1fa6c3c3.js
@@ -0,0 +1,2 @@
+!function(){var o=function(o,n){for(var l=0;l<o.length;l+=1)for(var r=l+1;r<o.length;r+=1)if(o[l]+o[r]===n)return[l,r]};console.log(o([2,7,11,15],9)),console.log(o([3,2,4],6)),console.log(o([3,3],6))}();
+//# sourceMappingURL=index.1fa6c3c3.js.map
---
+++ index.a5c0dd7c.js
@@ -0,0 +1,2 @@
+var o=function(o,l){for(let e=0;e<o.length;e+=1)for(let n=e+1;n<o.length;n+=1)if(o[e]+o[n]===l)return[e,n]};console.log(o([2,7,11,15],9)),console.log(o([3,2,4],6)),console.log(o([3,3],6));
+//# sourceMappingURL=index.a5c0dd7c.js.map
