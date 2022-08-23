073f934d0f72a59a4c39e3fc6ec74101a97ba86a
Maryna-Korbet <m22v86k@gmail.com>
2022-08-23 20:59:35 +0300

    Two Sum

--- src/js/twoSum.js
+++ src/js/twoSum.js
@@ -6,20 +6,19 @@
 //  * @param {number} target
 //  * @return {number[]}
 
-var twoSum = function(nums, target){
+var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i += 1) {
-        for (let j = i + 1; j < nums.length; j += 1){
-            if (nums[i] + nums[j] === target) {
+        for (let j = 0; j < nums.length; j += 1){
+            if (nums(i) + nums(j) === target) {
                 return [i,j];
             }
         }
     }
 };
 
-console.log(twoSum([2,7,11,15], 9));
-console.log(twoSum([3,2,4], 6));
-console.log(twoSum([3,3], 6));
-
+// export { twoSum };
+    
+console.log(twoSum([0, 1]));
 
 
 
